0d51fdbd3a680f2f9c0a56572fdfe9b2b4b80f8e
Roosevelt Burden <rooseveltburden@gmail.com>
2025-12-11 22:41:24 -0700

    try day 6 part 2

--- src/day_6.rs
+++ src/day_6.rs
@@ -16,7 +16,7 @@ impl crate::Day for Day6 {
 
 impl Day6 {
     fn solve_part_1(input: &str) -> u64 {
-        let (nums, ops) = Self::process_input(input);
+        let (nums, ops) = Self::process_input_part_1(input);
         let mut sum = 0;
         for (i, row) in nums.into_iter().enumerate() {
             let res = match ops[i] {
@@ -30,10 +30,37 @@ impl Day6 {
     }
 
     fn solve_part_2(input: &str) -> u64 {
+        let (nums, ops) = Self::process_input_part_2(input);
+        nums.iter()
+            .rev()
+            .map(|row| {
+                let max_len = row
+                    .iter()
+                    .map(|&n| {
+                        let (mut n, mut len) = (n, 0);
+                        while n > 0 {
+                            len += 1;
+                            n /= 10;
+                        }
+                        len
+                    })
+                    .max()
+                    .unwrap();
+                row.iter()
+                    .map(|n| {
+                        let mut s = n.to_string();
+                        while s.len() < max_len {
+                            s.push('0');
+                        }
+                        s
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .for_each(|s| println!("{s:?}"));
         0
     }
 
-    fn process_input(input: &str) -> (Vec<Vec<u64>>, Vec<&str>) {
+    fn process_input_part_1(input: &str) -> (Vec<Vec<u64>>, Vec<&str>) {
         let (top, bot) = input.rsplit_once("\n").expect("couldn't find a new line?");
         let nums_t: Vec<Vec<u64>> = top
             .lines()
@@ -48,6 +75,11 @@ impl Day6 {
             .collect();
         let ops = bot.split_whitespace().collect();
         (nums, ops)
+    }
+
+    fn process_input_part_2(input: &str) -> (Vec<Vec<u64>>, Vec<&str>) {
+        let (top, bot) = input.rsplit_once("\n").expect("couldn't find a new line?");
+        (vec![], vec![])
     }
 }
 
@@ -66,7 +98,7 @@ mod tests {
     #[test]
     fn test_part_2() {
         let data = include_str!("../data/day_6_test.txt");
-        let expected = 14;
+        let expected = 3263827;
         let actual = Day6::solve_part_2(data);
         assert_eq!(expected, actual);
     }
