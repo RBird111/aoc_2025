49cafe97e819784ef9352b7c28494d9ebee10e7c
Roosevelt Burden <rooseveltburden@gmail.com>
2025-12-14 11:36:22 -0700

    solve day 7 part 2

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]
 pub struct Day7;
 
 use std::cell::RefCell;
@@ -26,13 +25,13 @@ impl Day7 {
             .expect("couldn't find start position");
 
         let counter = Rc::new(RefCell::new(0));
-        let beam = Beam::new(start, counter.clone());
-        let mut curr_map = HashMap::from([(start, beam)]);
+        let beam = Beam::new(start, 1, counter.clone());
+        let mut map = HashMap::from([(start, beam)]);
 
-        while !curr_map.is_empty() {
-            curr_map = curr_map
+        while !map.is_empty() {
+            map = map
                 .values()
-                .filter_map(|b| b.increment(&grid))
+                .filter_map(|b| b.split(&grid))
                 .flatten()
                 .map(|b| (b.pos, b))
                 .collect();
@@ -42,7 +41,34 @@ impl Day7 {
     }
 
     fn solve_part_2(input: &str) -> u64 {
-        0
+        let grid = Day7::process_input(input);
+        let start = (0..grid.len())
+            .flat_map(|i| (0..grid.first().map_or(0, |row| row.len())).map(move |j| (i, j)))
+            .find(|&(i, j)| grid[i][j] == b'S')
+            .expect("couldn't find start position");
+
+        let counter = Rc::new(RefCell::new(1));
+        let beam = Beam::new(start, 1, counter.clone());
+        let mut map = HashMap::new();
+        let mut map_c = map.clone();
+        map.insert(start, beam);
+
+        while !map.is_empty() {
+            map.values()
+                .filter_map(|b| b.split(&grid))
+                .flatten()
+                .map(|b| (b.pos, b))
+                .for_each(|(pos, beam)| {
+                    map_c
+                        .entry(pos)
+                        .and_modify(|b| b.increment_local(beam.counter_local))
+                        .or_insert(beam);
+                });
+            std::mem::swap(&mut map, &mut map_c);
+            map_c.clear();
+        }
+
+        *counter.borrow()
     }
 
     fn process_input(input: &str) -> Vec<Vec<u8>> {
@@ -53,30 +79,48 @@ impl Day7 {
 #[derive(Clone, Debug)]
 struct Beam {
     pos: (usize, usize),
-    counter: Rc<RefCell<u64>>,
+    counter_global: Rc<RefCell<u64>>,
+    counter_local: u64,
 }
 
 impl Beam {
-    fn new(pos: (usize, usize), counter: Rc<RefCell<u64>>) -> Beam {
-        Beam { pos, counter }
+    fn new(pos: (usize, usize), counter_local: u64, counter_global: Rc<RefCell<u64>>) -> Beam {
+        Beam {
+            pos,
+            counter_global,
+            counter_local,
+        }
     }
 
-    fn increment(&self, grid: &[Vec<u8>]) -> Option<Vec<Beam>> {
+    fn split(&self, grid: &[Vec<u8>]) -> Option<Vec<Beam>> {
         let (x, y) = self.pos;
         match grid.get(x + 1)?.get(y)? {
-            b'.' => Some(vec![Beam::new((x + 1, y), self.counter.clone())]),
+            b'.' => Some(vec![Beam::new(
+                (x + 1, y),
+                self.counter_local,
+                self.counter_global.clone(),
+            )]),
             b'^' => {
-                *self.counter.borrow_mut() += 1;
+                *self.counter_global.borrow_mut() += self.counter_local;
                 Some(vec![
-                    Beam::new((x + 1, y.checked_sub(1)?), self.counter.clone()),
+                    Beam::new(
+                        (x + 1, y.checked_sub(1)?),
+                        self.counter_local,
+                        self.counter_global.clone(),
+                    ),
                     Beam::new(
                         (x + 1, (y + 1 < grid.first()?.len()).then_some(y + 1)?),
-                        self.counter.clone(),
+                        self.counter_local,
+                        self.counter_global.clone(),
                     ),
                 ])
             }
             _ => unreachable!(),
         }
+    }
+
+    fn increment_local(&mut self, amount: u64) {
+        self.counter_local += amount;
     }
 }
 
